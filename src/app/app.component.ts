f7be16161f90f15ab63f13a2ba23e5b9748994e6
dipeshsinha <1606270@kiit.ac.in>
2023-04-23 00:51:24 +0530

    skeleton images done. cursor pending

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio-2';
+  xAxisValue = 0
+  yAxisValue = 0
+  xHeightValue = 0
+  yWidthValue = 0
+  gridRangeWidth = 0.6
+  gridRangeHeight = 0.4
+
+  xaxis = document.querySelector<HTMLDivElement>('#xaxis')
+
+  onMouseMoved(event: MouseEvent) {
+
+
+    if( event.clientY < (window.innerHeight*this.gridRangeHeight) ) {
+      this.xAxisValue = event.clientY
+    } else {
+      this.xAxisValue = window.innerHeight*this.gridRangeHeight
+    }
+
+    if( event.clientX < (window.innerWidth*this.gridRangeWidth) ) {
+      this.yAxisValue = event.clientX
+    } else {
+      this.yAxisValue = window.innerWidth*this.gridRangeWidth
+    }
+
+    
+    // this.yAxisValue = event.clientX
+    this.xHeightValue = this.xAxisValue - (window.innerHeight * 0.075)
+    this.yWidthValue = this.yAxisValue - (window.innerWidth * 0.075)
+
+  }
+
 }
