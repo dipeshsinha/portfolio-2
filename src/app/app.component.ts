397383239a5e1e4df69159abd1b6e005fabda447
dipeshsinha <1606270@kiit.ac.in>
2023-04-23 01:01:45 +0530

    angular-cli-ghpages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +6,20 @@ import { Component, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   xAxisValue = 0
   yAxisValue = 0
   xHeightValue = 0
   yWidthValue = 0
-  gridRangeWidth = 0.6
-  gridRangeHeight = 0.4
+  bottomDivHeight = 0
+  bottomDivWidth = 0
+  mainContentDivHeight = 0
+  mainContentDivWidth = 0
+  gridRangeWidth = 0.35
+  gridRangeHeight = 0.3
 
-  xaxis = document.querySelector<HTMLDivElement>('#xaxis')
+
+
 
   onMouseMoved(event: MouseEvent) {
 
@@ -34,7 +40,11 @@ export class AppComponent {
     // this.yAxisValue = event.clientX
     this.xHeightValue = this.xAxisValue - (window.innerHeight * 0.075)
     this.yWidthValue = this.yAxisValue - (window.innerWidth * 0.075)
+    this.bottomDivHeight = window.innerHeight - this.xHeightValue - (window.innerHeight * 0.15)
+    this.bottomDivWidth = window.innerWidth - this.yWidthValue - (window.innerWidth * 0.15)
 
+    this.mainContentDivHeight = (window.innerHeight*(1 - this.gridRangeHeight)) - (window.innerHeight * 0.075)
+    this.mainContentDivWidth = (window.innerWidth*(1 - this.gridRangeWidth)) - (window.innerWidth * 0.075);
   }
 
 }
